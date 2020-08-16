b3bb0d4218a965d577c6ad9b58d10b7601f02da8
Jessica McMillen <jessicam@bread.home>
2020-08-16 20:46:34 +0100

    update

--- js/payment.js
+++ js/payment.js
@@ -3,8 +3,11 @@ function validateForm() {
     //Call functions from paymentValidation.js file, if all return true then redirect
     if (validCardName() && validCardNumber() && validMonth() && validYear() && validCVV()) {
         checkin = document.getElementById("checkIn").innerHTML = localStorage.getItem("checkInDate");
-        unavailableDatesPodsStored = JSON.parse(localStorage.getItem("unavailableDates"));
-        //add check in date to unavailable dates as only one pod
+        window.alert(checkin)
+
+        var unavailableDatesPodsStored = JSON.parse(localStorage.getItem("unavailableDates"));
+        window.alert(unavailableDatesPodsStored)
+            //add check in date to unavailable dates as only one pod
         unavailableDatesPodsStored.push(checkInDate);
         window.alert(unavailableDatesPodsStored)
             //update unavailable dates in local storage
