1a643cef71e8894d472d28babc8402abf4147012
Jessica McMillen <jessicam@bread.home>
2020-08-16 20:43:52 +0100

    update

--- js/payment.js
+++ js/payment.js
@@ -2,15 +2,17 @@
 function validateForm() {
     //Call functions from paymentValidation.js file, if all return true then redirect
     if (validCardName() && validCardNumber() && validMonth() && validYear() && validCVV()) {
+        checkin = document.getElementById("checkIn").innerHTML = localStorage.getItem("checkInDate");
+        unavailableDatesPodsStored = JSON.parse(localStorage.getItem("unavailableDates"));
+        //add check in date to unavailable dates as only one pod
+        unavailableDatesPodsStored.push(checkInDate);
+        window.alert(unavailableDatesPodsStored)
+            //update unavailable dates in local storage
+        localStorage.setItem("unavailableDates", JSON.stringify(unavailableDatesPodsStored));
         if (confirm("Confirm booking?")) { //Confirm pop up 
             window.location = "confirmation.html"; //redirect to confirmation page
             document.getElementById("paymentForm").reset(); //reset form
-            checkin = document.getElementById("checkIn").innerHTML = localStorage.getItem("checkInDate");
-            unavailableDatesPodsStored = JSON.parse(localStorage.getItem("unavailableDates"));
-            //add check in date to unavailable dates as only one pod
-            unavailableDatesPodsStored.push(checkInDate);
-            //update unavailable dates in local storage
-            localStorage.setItem("unavailableDates", JSON.stringify(unavailableDatesPodsStored));
+
         }
     }
 }
